a78943cbd0cbb2cf721c679645d17fede6647445
Peter Mauldin <pmauldin@snaptrends.com>
2016-02-20 14:47:20 -0600

    Working on getting car to jump

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class PlayerCar : MonoBehaviour {
 	public Transform transform;
 
-	public float acceleration = 9;
-	public float sideSpeed = 100;
-	public float initialVelocity = 100;
-	Vector3 velocity;
+	public float acceleration = 0.1f;
+	public float sideSpeed = 3;
+	public float initialVelocity = 0.5f;
+	public float maxJumpHeight = 0.6f;
+	public float jumpSpeed = 0.01f;
+	public Vector3 velocity;
+	Vector3 initialPos;
+	int jumpDirection = 1;
 
 	bool jumping = false;
 
 	// Use this for initialization
 	void Start () {
 		velocity = new Vector3(0, -initialVelocity, 0);
+		initialPos = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -26,15 +31,21 @@ public class PlayerCar : MonoBehaviour {
 			velocity.x = 0;
 		}
 
-		if (Input.GetKey(KeyCode.UpArrow) && !jumping) {
+		if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) && !jumping) {
 			jumping = true;
-
-
+			jumpDirection = 1;
 		}
 
-		if (transform.position.z < -4000) {
+		if (jumping && transform.position.y <= initialPos.y) {
+			jumping = false;
 			Vector3 pos = transform.position;
-			pos.z = 4000;
+			pos.y = initialPos.y;
+			transform.position = pos;
+		}
+
+		if (transform.position.z > 25) {
+			Vector3 pos = transform.position;
+			pos.z = -36;
 			transform.position = pos;
 		}
 	}
@@ -43,5 +54,13 @@ public class PlayerCar : MonoBehaviour {
 		velocity.y -= acceleration;
 		transform.Translate (velocity.x * Time.deltaTime, velocity.y * Time.deltaTime, velocity.z * Time.deltaTime);
 		Debug.Log ("Velocity is " + velocity);
+
+		if (jumping) {
+			if (jumpDirection == 1 && transform.position.y >= initialPos.y + maxJumpHeight) {
+				jumpDirection = -1;
+			}
+
+
+		}
 	}
 }
