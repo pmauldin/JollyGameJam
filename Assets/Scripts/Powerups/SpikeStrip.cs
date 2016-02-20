71cdbeb1fc22d67603123c1ea34be169379ca44a
Souvik Banerjee <souvik1997@gmail.com>
2016-02-20 15:01:12 -0600

    Collision detection with trees works

---
+++ Assets/Scripts/Powerups/SpikeStrip.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeStrip : MonoBehaviour {
+
+	public float VelocitySlow = 0.75f;
+	public float AccelSlow = 0.75f;
+	public float AccelSlowDuration = 3.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			var obj = col.gameObject.GetComponent<PlayerCar> ();
+			/*obj.setVelocity (VelocitySlow);
+			obj.setAcceleration (AccelSlow, AccelSlowDuration);*/
+			Debug.Log ("Collision");
+		}
+	}
+}
---
+++ Assets/Scripts/Powerups/Tree.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("Update");
+	
+	}
+
+	void FixedUpdate()
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log (col.gameObject);
+		if (col.gameObject.tag == "Player")
+		{
+			var obj = col.gameObject;
+			// obj.GetComponent<PlayerCar> ().setVelocity (0.75);
+			Debug.Log("Collision with Tree");
+		}
+	}
+}
