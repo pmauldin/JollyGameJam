ebba2e9f71a74fbeff4911637265811589aa3d82
nathanielballinger <nathanielballinger@utexas.edu>
2016-02-20 19:44:37 -0600

    Fence collision (not working)

---
+++ Assets/Scripts/FenceCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FenceCollision : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Car")
+        {
+            PlayerCar c = col.gameObject.GetComponent<PlayerCar>();
+            c.velocity = new Vector3(0, c.velocity.y, c.velocity.z);
+
+        }
+    }
+}
