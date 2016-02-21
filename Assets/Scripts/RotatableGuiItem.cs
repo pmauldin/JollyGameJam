2ce6bb50abac21ca6272df4bdd49c7cff27884a8
nathanielballinger <nathanielballinger@utexas.edu>
2016-02-21 03:42:59 -0600

    Stuff

--- Assets/Scripts/RotatableGuiItem.cs
+++ Assets/Scripts/RotatableGuiItem.cs
@@ -4,28 +4,24 @@ using System.Collections;
 
 public class RotatableGuiItem : MonoBehaviour {
 
-    public Texture2D texture = null;
-    public float angle = 0;
-    public Vector2 size = new Vector2(128, 128);
-    Vector2 pos = new Vector2(0, 0);
-    Rect rect;
-    Vector2 pivot;
-    void Start()
-    {
-        UpdateSettings();
-    }
-    void UpdateSettings()
-    {
-        pos = new Vector2(transform.localPosition.x, transform.localPosition.y);
-        rect = new Rect(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, size.x, size.y);
-        pivot = new Vector2(rect.xMin + rect.width * 0.5f, rect.yMin + rect.height * 0.5f);
-    }
+    public Texture2D dialTex;
+    public Texture2D needleTex;
+    public Vector2 dialPos;
+    public float topSpeed;
+    public float stopAngle;
+    public float topSpeedAngle;
+    public float speed;
+
     void OnGUI()
     {
-        if (Application.isEditor) { UpdateSettings(); }
-        Matrix4x4 matrixBackup = GUI.matrix;
-        GUIUtility.RotateAroundPivot(angle, pivot);
-        GUI.DrawTexture(rect, texture);
-        GUI.matrix = matrixBackup;
+
+        Vector2 centre = new Vector2(dialPos.x + dialTex.width / 2, dialPos.y + dialTex.height / 2);
+        float speedFraction = speed / topSpeed;
+        float needleAngle = Mathf.Lerp(stopAngle, topSpeedAngle, speedFraction);
+        Matrix4x4 savedMatrix = GUI.matrix;
+        GUI.DrawTexture(new Rect(dialPos.x, dialPos.y, dialTex.width, dialTex.height), dialTex);
+        GUIUtility.RotateAroundPivot(needleAngle, centre);
+        GUI.DrawTexture(new Rect(centre.x, centre.y - needleTex.height / 2, needleTex.width, needleTex.height), needleTex);
+        GUI.matrix = savedMatrix;
     }
 }
---
+++ NeedleScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NeedleScript : MonoBehaviour {
+    Texture2D dialTex;
+    Texture2D needleTex;
+    Vector2 dialPos;
+    float topSpeed;
+    float stopAngle;
+    float topSpeedAngle;
+    float speed;
+    void OnGUI() {
+        GUI.DrawTexture(Rect(dialPos.x, dialPos.y, dialTex.width, dialTex.height), dialTex);
+        var centre = Vector2(dialPos.x + dialTex.width / 2, dialPos.y + dialTex.height / 2);
+        var savedMatrix = GUI.matrix;
+        var speedFraction = speed / topSpeed;
+        var needleAngle = Mathf.Lerp(stopAngle, topSpeedAngle, speedFraction);
+        GUIUtility.RotateAroundPivot(needleAngle, centre);
+        GUI.DrawTexture(Rect(centre.x, centre.y - needleTex.height / 2, needleTex.width, needleTex.height), needleTex);
+        GUI.matrix = savedMatrix;
+    }
+}
