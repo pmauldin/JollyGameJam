405b5be4c97df3f81cdc106baf79083499efa77c
Jeannette Tang <jeannette.tang@utexas.edu>
2016-02-20 18:14:47 -0600

    loserboard template added

--- Assets/Button.js
+++ Assets/Button.js
@@ -3,3 +3,7 @@
 function Start_Game_Button () {
 	Application.LoadLevel("NateScene");
 }
+
+function Main_Menu_Button () {
+	Application.LoadLevel("Menu");
+}
---
+++ Assets/LoserBoardScrollView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class LoserBoardScrollView : MonoBehaviour {
+
+
+	void OnGUI() {
+		
+	}
+}
