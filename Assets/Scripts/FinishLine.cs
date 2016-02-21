38170695fa833f9e5a2ab242d2d8e3136fa3fe82
Jeannette Tang <jeannette.tang@utexas.edu>
2016-02-20 18:43:25 -0600

    added function to finishline collision

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollisionHandler : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "FinishLine") {
+			Application.LoadLevel("LoserBoard");
+		}
+	}
+}
